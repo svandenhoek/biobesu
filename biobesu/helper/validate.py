45ecd9ed646f9b364ee638010da87f12db577fe7
Sander van den Hoek <savandenhoek@gmail.com>
2021-02-23 10:34:22 +0100

    Reworked validation helper.

--- biobesu/helper/validate.py
+++ biobesu/helper/validate.py
@@ -3,45 +3,59 @@
 from shutil import which
 from os.path import isfile
 from os.path import isdir
+from os import access
+from os import R_OK
+from os import W_OK
+from os import X_OK
 
 
 def program(cmd):
     """
     Checks whether the command line (which should contain an application name) refers to a valid path. If it is an
-    existing application, does nothing. If the path is not valid, exits with an OSError indicating there is a missing
-    application.
+    existing application, does nothing. If the path is not valid, raises an OSError.
     :param cmd: the name of the application as how it would be called using the command line
     """
 
     if not which(cmd):
-        exit(OSError("Error: " + cmd + " is not available on this system."))
+        raise OSError("Error: " + cmd + " is not available on this system.")
 
 
-def file(parser, file_string, expected_extension):
+def file(file_string, expected_extension='', executable=False):
     """
-    Checks whether given path goes to a valid file. If not, throws a parser error.
-    :param parser: the parser used to throw an error
-    :param file_string: file path to check
-    :param expected_extension: the expected file extension
+    Checks whether given path goes to a valid file. If not, raises an OSError.
+    :param file_string: path to check whether it is a readable file
+    :param expected_extension: the expected file extension (default: '', which skips this check)
+    :param executable: whether file_string should be checked if it is executable (default: False)
     """
 
-    if not file_string.endswith(expected_extension):
-        parser.error('"' + file_string.split('/')[-1] + '" is not a .tsv file')
+    file_name = file_string.split('/')[-1]
     if not isfile(file_string):
-        parser.error('"' + file_string.split('/')[-1] + '" is not an existing file')
+        raise OSError('"' + file_name + '" is not a file')
+    if expected_extension != '' and not file_string.endswith(expected_extension):
+        raise OSError('"' + file_name + '" is not a ' + expected_extension + 'file')
+    if not access(file_string, R_OK):
+        raise OSError('"' + file_name + '" is not a readable file')
+    if executable and not access(file_string, X_OK):
+        raise OSError('"' + file_name + '" is not an executable file')
 
 
-def directory(parser, dir_string):
+def directory(dir_string, writable=True):
     """
-    Checks whether given path goes to an existing directory. If not, throws a parser error.
-    :param parser: the parser used to throw an error
+    Checks whether given path goes to an existing directory. If not, raises an OSError.
     :param dir_string: directory path to check
+    :param writable: check whether a directory is writable (default: True)
     """
 
-    # Strips slash on the end of an url.
-    dir_string = dir_string.rstrip("/")
+    # Strips slash on the end and re-adds it so input directories are always coherent.
+    dir_string = dir_string.rstrip('/') + '/'
+    dir_name = dir_string.split('/')[-2]
 
     if not isdir(dir_string):
-        parser.error('"' + dir_string.split('/')[-1] + '" is not a valid directory')
+        raise OSError('"' + dir_name + '" is not a directory')
+    if not access(dir_string, R_OK):
+        raise OSError('"' + dir_name + '" is not a readable directory')
+    if writable and not access(dir_string, W_OK):
+        raise OSError('"' + dir_name + '" is not a writable directory')
 
+    # Return coherent dir string.
     return dir_string
