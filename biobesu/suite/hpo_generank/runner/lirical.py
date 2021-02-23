6cde0e7e3b5593542849a1aeb7c0365fca4d0b55
Sander van den Hoek <savandenhoek@gmail.com>
2021-02-23 10:36:38 +0100

    Lirical script now runs lirical for each benchmark file.

--- biobesu/suite/hpo_generank/runner/lirical.py
+++ biobesu/suite/hpo_generank/runner/lirical.py
@@ -1,37 +1,76 @@
 #!/user/bin/env python3
 
-from os import mkdir
-from os.path import isdir
+from os import makedirs
+from subprocess import call
+from os import listdir
 from biobesu.helper import validate
 from biobesu.suite.hpo_generank.helper.converters import write_benchmarkdata_to_phenopackets
 
 def main(parser):
     args = __parse_command_line(parser)
-    __generate_phenopacket_files(args)
+    try:
+        phenopackets_dir = __generate_phenopacket_files(args)
+        lirical_output_dir = __run_lirical(args, phenopackets_dir)
+    except FileExistsError as e:
+        print("\nAn output file/directory already exists: " + e.filename + "\nExiting...")
 
 
 def __parse_command_line(parser):
     # Adds runner-specific command line.
-    parser.add_argument("--input", required=True, help="input benchmark file")
+    parser.add_argument("--jar", required=True, help="location of LIRICAL .jar file")
     parser.add_argument("--hpo", required=True, help="hpo.obo file")
+    parser.add_argument("--input", required=True, help="input tsv benchmark file")
     parser.add_argument("--output", required=True, help="directory to write output to")
 
+    parser.add_argument("--lirical_data", help="directory to write output to")
+    parser.add_argument("--lirical_exomiser", help="directory to write output to")
+
     # Processes command line.
-    args = parser.parse_args()
-    validate.file(parser, args.input, ".tsv")
-    validate.file(parser, args.hpo, ".obo")
-    args.output = validate.directory(parser, args.output)
+    try:
+        args = parser.parse_args()
+        validate.file(args.input, ".tsv")
+        validate.file(args.hpo, ".obo")
+        validate.file(args.jar, ".jar")
+        args.output = validate.directory(args.output)
+
+        if args.lirical_data is not None:
+            args.lirical_data = validate.directory(args.lirical_data)
+            validate.file(args.lirical_data + "Homo_sapiens_gene_info.gz")
+            validate.file(args.lirical_data + "hp.obo")
+            validate.file(args.lirical_data + "mim2gene_medgen")
+            validate.file(args.lirical_data + "phenotype.hpoa")
+        if args.lirical_exomiser is not None:
+            validate.directory(args.lirical_exomiser)
+    except OSError as e:
+        parser.error(e)
 
     return args
 
 
 def __generate_phenopacket_files(args):
-    phenopackets_dir = args.output + "/phenopackets/"
-
-    # Skips creating phenopackets if dir already exists.
-    if isdir(phenopackets_dir):
-        print("Phenopackets subdirectory found, skipping conversion of benchmark data.")
-        return
-
-    mkdir(phenopackets_dir)
+    phenopackets_dir = args.output + "phenopackets/"
+    makedirs(phenopackets_dir, exist_ok=False)
     write_benchmarkdata_to_phenopackets(args.input, phenopackets_dir, args.hpo)
+    return phenopackets_dir
+
+
+def __run_lirical(args, phenopackets_dir):
+    # Prepares output directory.
+    lirical_output_dir = args.output + "lirical_output/"
+    makedirs(lirical_output_dir, exist_ok=False)
+
+    # Digests optional arguments.
+    optional_arguments = ""
+    if args.lirical_data is not None:
+        optional_arguments += " -d " + args.lirical_data
+    if args.lirical_exomiser is not None:
+        optional_arguments += " -e " + args.lirical_exomiser
+
+    # Run tool for each input file.
+    for file in listdir(phenopackets_dir):
+        file_path = phenopackets_dir + file
+        run_output_dir = lirical_output_dir + file.strip(".json").split('/')[-1]
+        call("java -jar " + args.jar + " phenopacket -p " + file_path + " -o " + run_output_dir + " --tsv" +
+             optional_arguments, shell=True)
+
+    return lirical_output_dir
